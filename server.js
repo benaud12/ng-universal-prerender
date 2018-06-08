74416fb1cf7b042b5193fdda95e871fe0d64adef
michael.roger <michael.roger@wonga.com>
2018-06-08 15:16:36 +0100

    Adding express server for ng-universal rendering.

---
+++ server.js
@@ -0,0 +1,56 @@
+// These are important and needed before anything else
+require('zone.js/dist/zone-node');
+require('reflect-metadata');
+
+const { renderModuleFactory } = require('@angular/platform-server');
+const { enableProdMode } = require('@angular/core');
+
+const express = require('express');
+const { join } = require('path');
+const { readFileSync } = require('fs');
+
+// Faster server renders w/ Prod mode (dev mode never needed)
+enableProdMode();
+
+// Express server
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+const DIST_FOLDER = join(process.cwd(), 'dist');
+
+// Our index.html we'll use as our template
+const template = readFileSync(join(DIST_FOLDER, 'browser', 'index.html'))
+  .toString();
+
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main');
+const { provideModuleMap } = require('@nguniversal/module-map-ngfactory-loader');
+
+app.engine('html', (_, options, callback) => {
+  renderModuleFactory(AppServerModuleNgFactory, {
+    // Our index.html
+    document: template,
+    url: options.req.url,
+    // DI so that we can get lazy-loading to work differently (since we need it to just instantly render it)
+    extraProviders: [
+      provideModuleMap(LAZY_MODULE_MAP)
+    ]
+  }).then(html => {
+    callback(null, html);
+  });
+});
+
+app.set('view engine', 'html');
+app.set('views', join(DIST_FOLDER, 'browser'));
+
+// Server static files from /browser
+app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
+
+// All regular routes use the Universal engine
+app.get('*', (req, res) => {
+  res.render(join(DIST_FOLDER, 'browser', 'index.html'), { req });
+});
+
+// Start up the Node server
+app.listen(PORT, () => {
+  console.log(`Node server listening on http://localhost:${PORT}`);
+});
