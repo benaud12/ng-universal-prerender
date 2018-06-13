02c3ea8ae68ea41cfa33e8c8de1f2ec118fdf171
Michael Roger <bodgeuk12@gmail.com>
2018-06-13 01:45:47 +0100

    Enabled tests running in chrome headless and added AWS buildspec file.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,6 +25,17 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
     browsers: ['Chrome'],
     singleRun: false
   });
